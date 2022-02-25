914e3e6335ad56df5a2ba0f6ca6a0370e0ecbfb9
Arnar Gauti Ingason <arnarg@fastmail.com>
2022-02-25 22:16:39 +0100

    Fixing broken tests

--- spec/todotxt/init_spec.lua
+++ spec/todotxt/init_spec.lua
@@ -19,13 +19,13 @@ describe("TodoTxt", function()
 			local tasks = todotxt:get_tasks()
 			assert.are.same("A", tasks[1].priority)
 			assert.are.same("Do high priority task", tasks[1]:description())
-			assert.are.same("(A) Do high priority task", tasks[1]:string())
+			assert.are.same("(A) "..os.date("%Y-%m-%d", os.time()).." Do high priority task", tasks[1]:string())
 
 			assert.are.same("B", tasks[2].priority)
 			assert.are.same({"PersonalLife"}, tasks[2].projects)
 			assert.are.same({"Home"}, tasks[2].contexts)
 			assert.are.same("Call mom", tasks[2]:description())
-			assert.are.same("(B) Call mom +PersonalLife @Home", tasks[2]:string())
+			assert.are.same("(B) "..os.date("%Y-%m-%d", os.time()).." Call mom +PersonalLife @Home", tasks[2]:string())
 
 			assert.are.same({due = "2022-02-27"}, tasks[3].kv)
 			assert.are.same("Do task with date", tasks[3]:description())
@@ -33,8 +33,8 @@ describe("TodoTxt", function()
 		end)
 
 		it("should output correct todotxt string data", function()
-			local expected = "(A) Do high priority task\n" ..
-					"(B) Call mom +PersonalLife @Home\n" ..
+			local expected = "(A) "..os.date("%Y-%m-%d", os.time()).." Do high priority task\n" ..
+					"(B) "..os.date("%Y-%m-%d", os.time()).." Call mom +PersonalLife @Home\n" ..
 					"2022-02-25 Do task with date due:2022-02-27\n"
 
 			assert.are.same(expected, todotxt:build_lines())
